781a03b2843481e14eabffe343c20cbed7e71a07
Devmak1135 <136146865+Devmak1135@users.noreply.github.com>
2024-10-21 12:55:39 +0530

    pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,27 @@
+class pattern1{
+     public static void main(String args[])
+	 {
+		 int n=5;
+		 
+		 for(int i=0;i<=n;i++)
+		 {
+			 for(int j=1;j<=i;j++)
+			 {
+				 System.out.print("*");
+			 }
+		   System.out.println();
+		 }
+	 }
+}
+/*
+OUTPUT:-
+E:\Devcs5>javac pattern1.java
+
+E:\Devcs5>java pattern1
+
+*
+**
+***
+****
+*****
+*/
