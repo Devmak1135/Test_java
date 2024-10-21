d797e6ddea8c55e30cbf8ca7bf55f212e4fb2df4
Devmak1135 <136146865+Devmak1135@users.noreply.github.com>
2024-10-21 11:57:35 +0530

    Hello

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+class hello{
+     public static void main(String args[])
+	 {
+		 System.out.println("HELLO WORLD");
+	 }		 
+}
+/*
+OUTPUT:-
+E:\Devcs5>javac hello.java
+
+E:\Devcs5>java hello
+HELLO WORLD*/
