9cb226517d19b03f946a2c019cc587a880dd06f6
Devmak1135 <136146865+Devmak1135@users.noreply.github.com>
2024-10-21 12:54:29 +0530

    arth.java

---
+++ arth.java
@@ -0,0 +1,21 @@
+class arth{
+     public static void main(String args[])
+	 {
+		 int n1=Integer.parseInt(args[0]);
+		 int n2=Integer.parseInt(args[1]);
+		 
+		 System.out.println("Addition: "+(n1+n2));
+		 System.out.println("Multiply: "+(n1*n2));
+		 System.out.println("Division: "+(n1/n2));
+		 System.out.println("Subtraction: "+(n1-n2));
+	 }
+
+}
+OUTPUT:-
+E:\Devcs5>javac arth.java
+
+E:\Devcs5>java arth 50 30
+Addition: 80
+Multiply: 1500
+Division: 1
+Subtraction: 20
